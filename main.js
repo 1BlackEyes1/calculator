43cfd1038f4788137dd1325e867449d14606633c
pedro <blackeyes.pg@gmail.com>
2023-09-07 01:05:48 -0400

    javascript in progress ...

--- main.js
+++ main.js
@@ -3,47 +3,97 @@ const operators = document.querySelectorAll('.operator')
 const screen = document.querySelector('.screen');
 const screenP = document.querySelector('.screen-p');
 const equal = document.querySelector('.button__equal')
+const aC = document.querySelector('.reset')
 
 let firstNum = ''
 let secondNum = ''
 let ope = ''
-let result = ''
+
+aC.addEventListener('click',reset)
+
+equal.addEventListener('click', () =>{
+    secondNum = parseInt(screen.textContent)
+    screenP.textContent = ''
+    screen.textContent = operation(firstNum, ope, secondNum)
+
+    if(screen.textContent == ''){
+    screen.textContent = 0
+}
+    
+    btns.forEach(btn =>{
+        btn.addEventListener('click', onClick)
+        
+});
+});
+
+function reset(){
+    screen.textContent = 0
+    firstNum = ''
+    secondNum = ''
+    ope = ''
+    screenP.textContent = ''
+}
+
+function onClick(){
+    reset()
+    btns.forEach(btn =>{
+        btn.removeEventListener('click', onClick)
+        })
+    } ;
 
 btns.forEach(btn =>{
     btn.addEventListener('click', () =>{
         if(screen.textContent == 0){
             screen.textContent = ''
-        };
+        }
         screen.textContent += btn.textContent
+        console.log('write')
     });
 });
+
+//disable true para el punto
 
 operators.forEach(operator => {
     operator.addEventListener('click', () =>{
+        if(firstNum !== ''){
+            secondNum = parseInt(screen.textContent)
+            screenP.textContent = operation(firstNum, ope, secondNum)
+            firstNum = parseInt(screenP.textContent)
+            ope = operator.textContent
+            screenP.textContent += ` ${ope}`
+            screen.textContent = 0
+        }else{
         screenP.textContent = `${screen.textContent} ${operator.textContent}`
-        screen.textContent = ''
+        firstNum = parseInt(screen.textContent)
+        ope = operator.textContent
+        screen.textContent = 0}
+        
     });
 });
 
-equal.addEventListener('click', () =>{
-     
-});
 
 
-function add(a, b){
-    return a + b
+
+function operation(num1, sym, num2){
+    if(sym == '+'){
+      return num1 + num2
+    }
+
+    else if(sym == '-'){
+        return num1 - num2
+    }
+    
+    else if(sym == '*'){
+        return num1 * num2
+    }
+
+    else if(sym== '/'){
+        return num1 * num2
+    }
 }
 
-function substract(a, b){
-    return a - b
-}
 
-function multiply(a, b){
-    return a * b
-}
 
-function divide(a, b){
-    return a / b
-}
+
 
 
